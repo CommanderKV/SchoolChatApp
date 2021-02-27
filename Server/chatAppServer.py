b21b8e01a0632c59736666044a9e4c11cfc7c530
CommanderKV <kylervisser@gmail.com>
2021-02-27 16:06:06 -0500

    modified:   Server/chatAppServer.py

--- Server/chatAppServer.py
+++ Server/chatAppServer.py
@@ -393,7 +393,7 @@ def handle_client(client, username, hostname, client_addr):  # Takes client sock
 
     # tell everyone that the user joined the chat
     msg = f"{username} has joined the chat!"
-    print(f"Amount of pepole conected: {len(clients)+1}")
+    print(f"Amount of pepole conected: {len(clients)}")
     broadcast(bytes(msg, "utf8"))
 
     while True and checkForPulse():
@@ -498,6 +498,7 @@ def broadcast(msg, prefix="", msgTF=True):  # prefix is for name identification.
     delsocks = []
     for pos, sock in enumerate(clients):
         # print(f"Clients username: '{usernames_of_clients[pos]}'")
+        print(pos, len(clients_HeartBeats), len(usernames_of_clients), len(clients_HeartBeats[usernames_of_clients[pos]]))
         if clients_HeartBeats[usernames_of_clients[pos]][1].is_alive():
             sock.send(bytes(prefix, "utf8")+msg)
             # print(f"Sending: '{prefix+(msg.decode())}'")
